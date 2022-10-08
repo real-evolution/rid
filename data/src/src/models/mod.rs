fe4930bba32ec6636116f291e3329ebc18b28c81
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-08 09:54:02 +0300

    Add aliasees for models

---
+++ data/src/src/models/mod.rs
@@ -0,0 +1,4 @@
+pub use entity::user::Entity as User;
+pub use entity::oauth2_login_method::Entity as OAuthLoginMethod;
+pub use entity::crypto_wallet::Entity as CryptoWallet;
+pub use entity::transaction::Entity as Transaction;
