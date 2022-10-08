969b37845793ee3075610742f94e516e753f8c23
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-08 09:54:42 +0300

    Remove unneeded files

--- data/src/src/models/mod.rs
+++
@@ -1,4 +0,0 @@
-pub use entity::user::Entity as User;
-pub use entity::oauth2_login_method::Entity as OAuthLoginMethod;
-pub use entity::crypto_wallet::Entity as CryptoWallet;
-pub use entity::transaction::Entity as Transaction;
